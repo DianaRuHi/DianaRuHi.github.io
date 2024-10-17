7a8445d0f91ee7fb8a80deac2f86c50967c734e9
Diana <dianarh@rambler.ru>
2024-10-18 00:30:52 +0300

    Update counter.js

--- js/counter.js
+++ js/counter.js
@@ -1,7 +1,7 @@
 /*
  * jQuery.appear
  * http://code.google.com/p/jquery-appear/
- *
+ * 
  * Copyright (c) 2009 Michael Hixson
  * Licensed under the MIT license (http://www.opensource.org/licenses/mit-license.php)
 */
